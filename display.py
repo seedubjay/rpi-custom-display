c0650e1e54baa9def9d79e4550f5c2a831d89b03
seedubjay <cwj@CWJ-Macbook-Pro.lan>
2020-11-27 01:25:52 +0000

    delay power off

--- display.py
+++ display.py
@@ -178,8 +178,11 @@ def get_color_change(duration, brightness=0, temp=0):
             color_cache_time = datetime.datetime.now()
         color_cache[2] = min(max(color_cache[2]+int(brightness*65536),0),65535)
         color_cache[3] = min(max(color_cache[3]+temp,1500),9000)
+        if color_cache[2] > 0: light.set_power("on", True)
         light.set_color(color_cache, duration, True)
-        light.set_power("on" if color_cache[2] > 0 else "off", True)
+        if color_cache[2] == 0:
+            time.sleep(duration+.1)
+            light.set_power("off", True)
     return f
 
 buttons = [
